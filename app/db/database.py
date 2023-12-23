eb3bf5919b953045adfe4620325caf35e3fa4b5c
sangjuncha.dev <sangjuncha.dev@gmail.com>
2023-12-23 17:55:47 +0900

    :hammer: Update apply scoped_session

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,8 @@
 import logging
+from asyncio import current_task
 from collections.abc import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker, async_scoped_session
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import DeclarativeBase
 
@@ -12,23 +13,22 @@ logger = logging.getLogger(__name__)
 
 async_engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 async_session = async_sessionmaker(bind=async_engine, autoflush=False, expire_on_commit=False)
+scoped_session = async_scoped_session(session_factory=async_session, scopefunc=current_task)
 
 
 class Base(DeclarativeBase):
     pass
 
 
-async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
-    import asyncio
-
-    async with async_session.begin() as session:
+async def get_db_session() -> AsyncGenerator[async_scoped_session, None]:
+    async with scoped_session() as session:
         try:
             yield session
-            await session.commit()
+
         except SQLAlchemyError as error:
             logger.error(error, exc_info=True)
             await session.rollback()
             raise
+
         finally:
-            await session.close()
-            await asyncio.shield(session.close())
+            await async_engine.dispose()
